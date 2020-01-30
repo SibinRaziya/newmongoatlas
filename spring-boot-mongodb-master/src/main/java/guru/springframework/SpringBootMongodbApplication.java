39c590e4a997b136001fdb45652c6d073e115bec
SibinRaziya <38701451+SibinRaziya@users.noreply.github.com>
2020-01-30 11:58:57 +0530

    Update SpringBootMongodbApplication.java

--- spring-boot-mongodb-master/src/main/java/guru/springframework/SpringBootMongodbApplication.java
+++ spring-boot-mongodb-master/src/main/java/guru/springframework/SpringBootMongodbApplication.java
@@ -1,5 +1,12 @@
 package guru.springframework;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import guru.springframework.repositories.ProductRepository;
 
 @SpringBootApplication
+@Configuration
 public class SpringBootMongodbApplication {
 
 	@Autowired
@@ -17,5 +25,15 @@ public class SpringBootMongodbApplication {
 
 	}
 
-	
+	@Bean
+	public MongoDatabase mongoDatabase() {
+
+		MongoClientURI uri = new MongoClientURI(
+				"mongodb+srv://sibin:Boat786#@cluster0-vgzxw.mongodb.net/test?retryWrites=true&w=majority");
+
+		MongoClient mongoClient = new MongoClient(uri);
+		MongoDatabase database = mongoClient.getDatabase("test");
+		return database;
+
+	}
 }
